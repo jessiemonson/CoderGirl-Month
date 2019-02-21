eb395539019578c091996331f69bd9b262948b76
Jessie Monson <jessiemonson1980@gmail.com>
2019-02-20 21:26:21 -0600

    month final

--- Month/Program.cs
+++ Month/Program.cs
@@ -9,48 +9,56 @@ namespace Month
             Console.Write("Enter the number of the month: ");
             string userValue = Console.ReadLine();
             string monthName = "";
-           {
-            if(userValue == "1");
-                monthName = January; 
-                                        
-            if(userValue == "2");
-                monthName = February; 
-                                        
-            else if (userValue == "3")
-                monthName = March; 
-                                        
-            else if (userValue == "4");
-                monthName = April; 
-                                        
-            else if (userValue == "5");
-                monthName = May; 
-                                        
-            else if (userValue == "6");
-                monthName = June; 
-           
-            else if (userValue == "7");
-                monthName = July; 
-           
-            else if (userValue == "8");
-                monthName = August; 
-           
-            else if (userValue == "9");
-                monthName = September; 
-           
-            else if (userValue == "10");
-                monthName = October; 
-                                             
-            else if (userValue == "11");
-                monthName = November; 
-                                          
-           
-            else if (userValue == "12");
-                monthName = December; 
-           }                               
-            string monthName = string.Empty;
+            
+                if (userValue == "1") { 
+                monthName = "January"; }
+            
+            else if (userValue == "2") { 
+            monthName = "February"; }
+                                       
+            else if (userValue == "3") { 
+                monthName = "March"; }
+
+            else if (userValue == "4")
+            {
+                monthName = "April";
+            }
+            else if (userValue == "5")
+            {
+                monthName = "May";
+            }
+            else if (userValue == "6")
+            {
+                monthName = "June";
+            }
+            else if (userValue == "7")
+            {
+                monthName = "July";
+            }
+            else if (userValue == "8")
+            {
+                monthName = "August";
+            }
+            else if (userValue == "9")
+            {
+                monthName = "September";
+            }
+            else if (userValue == "10")
+            {
+                monthName = "October";
+            }
+            else if (userValue == "11")
+            {
+                monthName = "November";
+            }
+            else if (userValue == "12")
+            {
+                monthName = "December";
+            }
+            
 
 
-            Console.WriteLine($"The name of the month you entered is : (monthName)");
+            Console.WriteLine($"The name of the month you entered is : {monthName}");
 
             Console.ReadLine();
         }   
